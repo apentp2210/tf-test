c5c6c56fe22e6abbc128f307c7821848eaa645d9
An <an.trannhat@gmail.com>
2021-07-24 09:23:03 +0700

    update default vpc

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ data "aws_ssm_parameter" "linux" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
+  manage_default_vpc = true  
   azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
   cidr = "10.0.0.0/16"
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
